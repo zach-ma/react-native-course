32deb75b65d122eb9212f4db0d355e265d183599
MJX-Git <63674024+MJX-Git@users.noreply.github.com>
2021-07-15 16:21:02 -0400

    install packages, create boiler plates

---
+++ tracks-app/src/screens/AccountScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const AccountScreen = () => {
+  return (
+    <View>
+      <Text>account screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default AccountScreen;
---
+++ tracks-app/src/screens/TrackDetailScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const TrackDetailScreen = () => {
+  return (
+    <View>
+      <Text>TrackDetail screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackDetailScreen;
